6a000cea80b4a7ec399b821089cebf3f52da8946
Raj Patel <raj.patel4@mavs.uta.edu>
2023-07-04 20:02:44 -0500

    resize and rotate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 
-img = cv2.imread('assets/landing.jpg')
+img = cv2.imread('assets/landing.jpg', 1)
+img = cv2.resize(img, (0, 0), fx= 0.5, fy= 0.5)
+img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
 
 cv2.imshow("Image", img)
