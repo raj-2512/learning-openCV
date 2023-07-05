9a276a52ee9fdcbf78ae1e621874a38095e2f157
Raj Patel <raj.patel4@mavs.uta.edu>
2023-07-05 09:35:13 -0500

    copy and paste pixals

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import cv2
+import random
 
-img = cv2.imread('assets/landing.jpg', 1)
-img = cv2.resize(img, (0, 0), fx= 0.5, fy= 0.5)
-img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+img = cv2.imread('assets/landing.jpg', -1)
+
+tag = img[500:700, 600:900]
+img[100:300, 650:950] = tag
 
 
-cv2.imshow("Image", img)
+cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
